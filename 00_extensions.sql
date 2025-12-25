043c512142bc5530080e774f1dc335e131f70731
RX-kabir <156705525+RX-kabir@users.noreply.github.com>
2025-12-25 12:44:25 +0600

    Remove all SQL files

--- 00_extensions.sql
+++
@@ -1,8 +0,0 @@
--- 00_extensions.sql
--- Optional extensions. Uncomment what you need.
-
--- If you want UUID primary keys (not required when using BIGSERIAL):
--- CREATE EXTENSION IF NOT EXISTS pgcrypto;  -- provides gen_random_uuid()
-
--- If you later use geospatial routing (lat/lng + distance), consider PostGIS:
--- CREATE EXTENSION IF NOT EXISTS postgis;
--- 01_schema.sql
+++
@@ -1,5 +0,0 @@
--- 01_schema.sql
-CREATE SCHEMA IF NOT EXISTS smartwaste;
-
--- Optional: in psql sessions you can set this so you don't need smartwaste.<object>
--- SET search_path TO smartwaste, public;
